21b1a076d4758f3bc4c44d5aa050036fa15d92fa
Tlon9 <21520331@gm.uit.edu.vn>
2024-07-14 15:50:24 +0700

    Update transfer/display-result

--- travelowkey/transfer/views.py
+++ travelowkey/transfer/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def transfer_search(request):
     return render(request,'transfer/transfer-search/index.html')
+
+def transfer_results(request):
+    return render(request,'transfer/transfer-display/index.html')
