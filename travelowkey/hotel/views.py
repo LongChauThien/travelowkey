d0c69795c6d4bc18370865b92286d85fdf7d4069
Tlon9 <21520331@gm.uit.edu.vn>
2024-07-14 15:17:34 +0700

    Update hotel/display-result

--- travelowkey/hotel/views.py
+++ travelowkey/hotel/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def hotel_search(request):
     return render(request, 'hotel/hotel-search/index.html')
+
+def hotel_results(request):
+    return render(request, 'hotel/hotel-display/index.html')
