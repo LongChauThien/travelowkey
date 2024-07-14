e92b1a451da789da3045cfc195f8148b78f10812
Tlon9 <21520331@gm.uit.edu.vn>
2024-07-14 15:39:00 +0700

    Update bus/display-result

--- travelowkey/bus/urls.py
+++ travelowkey/bus/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import bus_search
+from .views import bus_search, bus_result
 
 urlpatterns = [
-    path('',bus_search,name='bus_search')
+    path('search/',bus_search,name='bus_search'),
+    path('results/',bus_result,name='bus_result')
 ]
