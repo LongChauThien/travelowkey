e21d00b0a1e26566670271f4d774060ef82383aa
Tlon9 <21520331@gm.uit.edu.vn>
2024-07-15 14:59:07 +0700

    Update hotel-search/js

---
+++ travelowkey/static/hotel/hotel-search/js/hotel-search.js
@@ -0,0 +1,81 @@
+let HotelSearchInfo = {
+    location: '',
+    checkinDate: '',
+    checkoutDate: '',
+    adult: 1,
+    child: 0,
+    room: 1,
+}
+
+const featureHotelSearch = document.getElementById('feature__hotel-search');
+const hotelConfig = featureHotelSearch.querySelector('.hotel-search__config');
+const guestAndRoomQuantity = hotelConfig.querySelector('.booking-info__guest-and-room-quantity');
+const guestAndRoomQuantityInfo = guestAndRoomQuantity.querySelector('.guest-and-room-quantity__info');
+const guestAndRoomQuantityDropdownPanel = guestAndRoomQuantity.querySelector('.passenger-quantity__dropdown-panel');
+const guestAndRoomQuantityItems = guestAndRoomQuantityDropdownPanel.querySelectorAll('.dropdown-panel__item');
+const guestAndRoomQuantityConfirmBtn = guestAndRoomQuantityDropdownPanel.querySelector('.dropdown-panel__confirm-btn');
+
+const hotelSearchForm = featureHotelSearch.querySelector('#hotel-search__search-form');
+const locationInput = hotelSearchForm.querySelector('#hotel-search__location');
+const checkinDateInput = hotelSearchForm.querySelector('#hotel-search__checkin-date');
+const checkoutDateInput = hotelSearchForm.querySelector('#hotel-search__checkout-date');
+const submitBtn = hotelSearchForm.querySelector('#search-form__submit-btn--hotel');
+
+guestAndRoomQuantity.addEventListener('click', () => {
+    guestAndRoomQuantityDropdownPanel.classList.toggle('hide');
+});
+
+
+guestAndRoomQuantityDropdownPanel.addEventListener('click', (e) => {
+    e.stopPropagation();
+});
+
+document.addEventListener('click', (e) => {
+    if (guestAndRoomQuantityDropdownPanel.classList.contains('hide')) return;
+    if (guestAndRoomQuantity.contains(e.target)) return;
+    guestAndRoomQuantityDropdownPanel.classList.add('hide');
+});
+
+guestAndRoomQuantityItems.forEach(item => {
+    const passengerType = item.dataset.passengerType;
+    const setQuantity = item.querySelector('.item__set-quantity');
+    const quantity = setQuantity.querySelector('.set-quantity__quantity');
+    const increaseBtn = setQuantity.querySelector('.set-quantity__icon.increase');
+    const decreaseBtn = setQuantity.querySelector('.set-quantity__icon.decrease');
+
+    quantity.innerText = HotelSearchInfo[passengerType];
+
+    increaseBtn.addEventListener('click', () => {
+        if (HotelSearchInfo[passengerType] < 9) {
+            HotelSearchInfo[passengerType]++;
+            quantity.innerText = HotelSearchInfo[passengerType];
+        }
+    });
+
+    decreaseBtn.addEventListener('click', () => {
+        if ((passengerType != 'adult' && HotelSearchInfo[passengerType] > 0) || (passengerType == 'adult' && HotelSearchInfo[passengerType] > 1)) {
+            HotelSearchInfo[passengerType]--;
+            quantity.innerText = HotelSearchInfo[passengerType];
+        }
+    });
+});
+
+guestAndRoomQuantityConfirmBtn.addEventListener('click', () => {
+    guestAndRoomQuantityDropdownPanel.classList.add('hide');
+    guestAndRoomQuantityInfo.querySelector(".text").innerText = `${HotelSearchInfo.adult} nguoi lon, ${HotelSearchInfo.child} tre em, ${HotelSearchInfo.room} phong`;
+});
+
+
+function getToday() {
+    const today = new Date()
+    const date = today.getDate()
+    const month = today.getMonth() + 1
+    const year = today.getFullYear()
+    return `${year}-${month < 10 ? "0" + month : month}-${date < 10 ? "0" + date : date}`
+}
+
+const today = getToday()
+checkinDateInput.value = today
+checkoutDateInput.value = today
+checkinDateInput.min = today
+checkoutDateInput.min = today
